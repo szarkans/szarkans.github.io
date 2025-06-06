22e63b9f5b6e0e31b5a4edf777e2c90797801b42
Серёжа <150809885+szarkans@users.noreply.github.com>
2025-06-06 11:40:11 +0300

    discord links

--- .vitepress/theme/HomeFaq.vue
+++ .vitepress/theme/HomeFaq.vue
@@ -63,7 +63,7 @@ const faqItems = [
   },
   {
     title: 'Как попасть на сервер?',
-    content: 'Пока что никак - мы на межсезонье до 01.06.2025! Но обязательно зайди в наш <a href="https://discord.gg/catcraftmc">Discord</a> или <a href="https://t.me/catcraftmc_tg">Telegram</a> чтобы быть в курсе новостей!'
+    content: 'Пока что никак - мы на межсезонье до 01.06.2025! Но обязательно зайди в наш <a href="https://discord.gg/6f3FwFRJWC">Discord</a> или <a href="https://t.me/catcraftmc_tg">Telegram</a> чтобы быть в курсе новостей!'
   },
   {
     title: 'Какая валюта сервера?',
