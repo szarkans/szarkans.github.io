71a7782907a2e7a186a622bdfd30685c3014ffba
Sergei Shatrov <150809885+szarkans@users.noreply.github.com>
2026-06-06 11:06:52 +0300

    chore: яндекс.метрика

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -170,7 +170,24 @@ export default defineConfig({
   base: '/',
 
   transformHead: ({ pageData }) => {
-    const head = [];
+    const head = head: [
+      [
+        'script',
+        { type: 'text/javascript' },
+        `(function(m,e,t,r,i,k,a){
+          m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+        })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=107063260', 'ym');
+        ym(107063260, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});`
+      ],
+      [
+        'noscript',
+        {},
+        '<div><img src="https://mc.yandex.ru/watch/107063260" style="position:absolute; left:-9999px;" alt="" /></div>'
+      ]
+    ];
     const siteUrl = 'https://wiki.catcraftmc.ru';
 
     const pageTitle = pageData.title || 'Кошкокрафт';
